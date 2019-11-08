0cae5872bcbeeb7f5270b12b5372e9531168decb
joliang17 <47235014+joliang17@users.noreply.github.com>
2019-11-07 21:58:46 -0800

    Add configuration file

--- Code/AWS File/Download_Data_Boto3.py
+++ Code/AWS File/Download_Data_Boto3.py
@@ -1,25 +1,29 @@
+import os
 import ssl
 import logging
 import boto3
 from botocore.exceptions import ClientError
 import pandas as pd
-# from ConfigParser import SafeConfigParser
+import configparser
 
-# parser = SafeConfigParser()
-# parser.read('aws.config')
+cwd = os.path.dirname(os.path.realpath(__file__))
+FilePath = cwd + '\\aws.config'
 
-# Access_key_ID = parser.get('bug_tracker', 'Access_key_ID')
-# Secret_access_key = parser.get('bug_tracker', 'Secret_access_key')
-# Region_Name = parser.get('bug_tracker', 'Region_Name')
+config = configparser.ConfigParser()
+config.read(FilePath)
+
+Access_key_ID = config.get('AWS_Credentials', 'Access_key_ID')
+Secret_access_key = config.get('AWS_Credentials', 'Secret_access_key')
+Region_Name = config.get('AWS_Credentials', 'Region_Name')
 
 start_byte = 0
 stop_byte=1000000
 
 client = boto3.client(
     's3',
-    aws_access_key_id=,
-    aws_secret_access_key=,
-    region_name='us-west-2'
+    aws_access_key_id=Access_key_ID,
+    aws_secret_access_key=Secret_access_key,
+    region_name=Region_Name
 )
 Objects = client.get_object(Bucket='rippled-fullhistory', Key='08232019/transaction.db', Range='bytes={}-{}'.format(start_byte, stop_byte))
 # Objects = client.get_object(Bucket='rippled-fullhistory', Key='08232019/transaction.db')
@@ -30,8 +34,3 @@ with open('Temp.db', 'wb') as w:
     w.write(X1)
 
 X1
-# response = s3.list_buckets()
-
-# print('Existing buckets:')
-# for bucket in response['Buckets']:
-#     print(f'  {bucket["Name"]}')
